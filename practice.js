6a8f3ba04b752bd9c1c6e9f38197003e1038da29
Brennan John Baker <bakerbj@s.dcsdk12.org>
2019-11-08 08:32:22 -0700

    practice stuff

---
+++ practice.js
@@ -0,0 +1,33 @@
+watermelon.style.background = "Ff0000";
+
+watermelon.innerHTML = "Banana";
+
+watermelon.innerHTML = "<p><strong>Banana</strong></p>";
+
+let fruit = "Apple"
+let x = 0
+let fruits = ["Apple", "Banana", "Watermelon"];
+
+// while (x < fruits.length()) {
+//     if (fruits[x] === "Banana") {
+//         list.append("BBBBBBBBBB");
+//     } else {
+//     list.append(fruits[x]);
+// } 
+// }
+
+// setTimeout(() => {
+//     watermelon.innerHTML = "<p><strong>" + fruit + "</strong></p>";
+//     console.log(watermelon);
+// },2000);
+
+
+let addFruit = function() {
+    watermelon.append(fruit);
+};
+
+addFruit("Strawberry");
+addFruit("Strawberry");
+addFruit("Strawberry");
+addFruit("Strawberry");
+addFruit("Strawberry");git st
